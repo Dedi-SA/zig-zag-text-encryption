2a2057b217a975e5d9fe2961999ae63748275229
Dedi-SA <coinfoner@gmail.com>
2024-10-11 21:29:35 +0700

    renaming variable

--- js/web-page-support/generating-mirror-type-inner-html.js
+++ js/web-page-support/generating-mirror-type-inner-html.js
@@ -1,11 +1,11 @@
 // Generate inner htmlMembuat daftar 'value' untuk tempatJenisCermin
-for (let i = 0; i < hurufKapital.length; i++) {
+for (let i = 0; i < get.capitalLetters().length; i++) {
     if (i >= CERMIN.length ) { // Berenti jika indeks lebih atau sama dengan panjang Cermin
         break;
     }
 
     let option_jenisCermin             = document.createElement('option');
         option_jenisCermin.value       = i;
-        option_jenisCermin.innerHTML   = hurufKapital[i];
+        option_jenisCermin.innerHTML   = get.capitalLetters()[i];
     tempatJenisCermin.appendChild(option_jenisCermin);
 }
