ff2329810ed8be3e0cd7e0843b98cb31041bd001
Dedi-SA <coinfoner@gmail.com>
2024-10-11 00:19:29 -0700

    renaming parameter of get.id() function

--- js/encryptor.js
+++ js/encryptor.js
@@ -60,19 +60,19 @@
           ];
 
     const get = {
-        id: (karakter, selectedKey) => { // Untuk mendapatkan nomor indeks dari string yang dikirim
-            if (is.str(karakter) && karakter.length === 1)
-                return KUNCI.indexOf(karakter, 0);
+        id: (theString, theList) => { // Untuk mendapatkan nomor indeks dari string yang dikirim
+            if (is.str(theString) && theString.length === 1)
+                return theList.indexOf(theString, 0);
             else {
                 console.log('Kesalahan : Fungsi get.id() menerima argumen yang tidak valid');
-                console.log('Jenis : ' + typeof karakter);
+                console.log('Jenis : ' + typeof theString);
 
-                if (is.str(karakter)) { // Jika string
-                    console.log('String : ' + ( karakter.length < 1 ? '<-kosong->' : (karakter === ' ' ? '<-spasi->' : karakter) ));
-                    console.log('Panjang string : ' + karakter.length);
+                if (is.str(theString)) { // Jika string
+                    console.log('String : ' + ( theString.length < 1 ? '<-kosong->' : (theString === ' ' ? '<-spasi->' : theString) ));
+                    console.log('Panjang string : ' + theString.length);
                 }
                 else { // Jika bukan string
-                    console.log('Nilai : ' + karakter);
+                    console.log('Nilai : ' + theString);
                 }
 
                 console.log('')
