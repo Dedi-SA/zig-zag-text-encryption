ce832ab9fb4dd4e782690087fd8802504c7dfd09
Dedi-SA <coinfoner@gmail.com>
2024-10-12 23:58:57 +0700

    add new function: isValidKey()

--- js/the-encryptor/key.js
+++ js/the-encryptor/key.js
@@ -1,3 +1,13 @@
 const keyStandartLength = 94, // The required number of elements needed for a valid KEY
 
-      KEY = [...'Pr#j["I4YRo.bW;^$v0_h8e`qL}J?u<G1%n]DA7:)N9~Vc-X*tOmi zfx=U{a&6T+EQ\'!2lK|HF>@Sgyk,Ms3dw/Bp(5CZ'];
+      KEY = [...'Pr#j["I4YRo.bW;^$v0_h8e`qL}J?u<G1%n]DA7:)N9~Vc-X*tOmi zfx=U{a&6T+EQ\'!2lK|HF>@Sgyk,Ms3dw/Bp(5CZ'],
+
+      // To Check is variable 'KEY' is valid or not
+      isValidKey = theKey => {
+            if (Array.isArray(theKey) && theKey.every(e => is.str(e))) {
+                
+                return noDuplicate(theKey).length === keyStandartLength;
+            }
+            else
+                return false;
+      }
