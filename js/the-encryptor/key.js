bb22d786272aa1b69d0c1eb680438d44edec518c
Dedi-SA <coinfoner@gmail.com>
2024-10-12 16:05:47 +0700

    add new variable: keyStandardLength

--- js/the-encryptor/key.js
+++ js/the-encryptor/key.js
@@ -1,1 +1,3 @@
-const KEY = [...'Pr#j["I4YRo.bW;^$v0_h8e`qL}J?u<G1%n]DA7:)N9~Vc-X*tOmi zfx=U{a&6T+EQ\'!2lK|HF>@Sgyk,Ms3dw/Bp(5CZ'];
+const keyStandartLength = 94, // The required number of elements needed for a valid KEY
+
+      KEY = [...'Pr#j["I4YRo.bW;^$v0_h8e`qL}J?u<G1%n]DA7:)N9~Vc-X*tOmi zfx=U{a&6T+EQ\'!2lK|HF>@Sgyk,Ms3dw/Bp(5CZ'];
