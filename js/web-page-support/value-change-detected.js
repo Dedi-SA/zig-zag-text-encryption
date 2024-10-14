cc897f05e5af071385a3236806db463c2d914fed
Dedi-SA <coinfoner@gmail.com>
2024-10-14 14:50:01 +0700

    minor refactoring

--- js/web-page-support/value-change-detected.js
+++ js/web-page-support/value-change-detected.js
@@ -1,35 +1,38 @@
-//         // Simplified element
+        // Simplified element
 const   tempatStatusEnkripsi   = get.element('#tempatStatusEnkripsi'),
-        tempatPassword         = get.element('#tempatPassword');
+        tempatPassword         = get.element('#tempatPassword'),
+        tempatInput            = get.element('#tempatInput'),
+        tempatOutput           = get.element('#tempatOutput');
 
 const   inputElements = document.querySelectorAll('.value-input'),
         changeElements = document.querySelectorAll('.value-change'),
         dblClickElements = document.querySelectorAll('.double-click'),
         inputPasswordElements = document.querySelectorAll('.password-input');
-
+        
+const   startTheProcess = () => {
+            console.log(tempatInput.value)
+            tempatOutput.value = ZZTE(tempatInput.value);
+            resultSpaceWarning();
+        };
 
         inputElements.forEach(theElement => {
-            theElement.addEventListener('input', function() {
-                tempatOutput.value = ZZTE();
-            });
-        });
+            theElement.addEventListener('input', startTheProcess);
+        })
 
         changeElements.forEach(theElement => {
-            theElement.addEventListener('change', function() {
-                tempatOutput.value = ZZTE();
-            });
-        });
+            theElement.addEventListener('change', startTheProcess)
+        })
 
         dblClickElements.forEach(theElement => {
             theElement.addEventListener('dblclick', function() {
                 tempatStatusEnkripsi.value = tempatStatusEnkripsi.value === 'enkripsi' ? 'dekripsi' : 'enkripsi';
-                tempatOutput.value = ZZTE();
-            });
-        });
+                startTheProcess();
+            })
+        })
 
         inputPasswordElements.forEach(theElement => {
             theElement.addEventListener('input', function() {
                 tempatPassword.value = [...specialGet.password()].filter(e => generalString.includes(e)).join('');
-                tempatOutput.value = ZZTE();
-            });
-        });
+                startTheProcess()
+            })
+        })
