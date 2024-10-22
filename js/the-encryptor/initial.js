6cf35a27b71db29fcb49bf083857d4c8627ff993
Dedi-SA <coinfoner@gmail.com>
2024-10-22 08:06:18 +0700

    step 1 (filtering) moved to initial.js

--- js/the-encryptor/initial.js
+++ js/the-encryptor/initial.js
@@ -1,3 +1,21 @@
+// Step 1 : Filtering
 const ZZTE = (theText, thePassword) => {
-    return zztEncryptor(theText, thePassword);
+    if (!is.str(theText)) {
+        invArg('zztEncryptor()');
+        console.log('The input must be string');
+    }
+    else {
+        if (theText.length === 0) {
+            return theText;
+        }
+        else {
+            if (typeof thePassword !== 'string') {
+                invArg('zztEncryptor()');
+                console.log('The password must be string');
+            }
+            else {
+                return mulaiEnkripsiDekripsi([...theText], thePassword);
+            }
+        }
+    }
 };
