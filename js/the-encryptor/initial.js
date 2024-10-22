9435801409ab5bbf32ec75d5e44c66eb4905e7b5
Dedi-SA <coinfoner@gmail.com>
2024-10-22 20:20:13 +0700

    add the condition if password length exceed 20

--- js/the-encryptor/initial.js
+++ js/the-encryptor/initial.js
@@ -13,6 +13,9 @@ const ZZTE = (theText, thePassword, status) => {
                 invArg('ZZTE()');
                 console.log('The password must be string');
             }
+            else if (thePassword.length > 20) {
+                thePassword = thePassword.slice(0, 19);
+            }
             else {
                 return mulaiEnkripsiDekripsi([...theText], thePassword, status);
             }
