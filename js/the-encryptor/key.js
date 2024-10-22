11a39ea3650d5bae0528bbe1bdaaad6053f12a65
Dedi-SA <coinfoner@gmail.com>
2024-10-22 22:14:33 +0700

    add rule on top

--- js/the-encryptor/key.js
+++ js/the-encryptor/key.js
@@ -1,6 +1,7 @@
-const keyStandartLength = 94, // The required number of elements needed for a valid KEY
+// Rule
+const keyStandartLength = 94; // The required number of elements needed for a valid KEY
 
-      KEY = [...'Pr#j["I4YRo.bW;^$v0_h8e`qL}J?u<G1%n]DA7:)N9~Vc-X*tOmi zfx=U{a&6T+EQ\'!2lK|HF>@Sgyk,Ms3dw/Bp(5CZ'],
+const KEY = [...'Pr#j["I4YRo.bW;^$v0_h8e`qL}J?u<G1%n]DA7:)N9~Vc-X*tOmi zfx=U{a&6T+EQ\'!2lK|HF>@Sgyk,Ms3dw/Bp(5CZ'],
 
       // To Check is variable 'KEY' is valid or not
       isValidKey = theKey => {
