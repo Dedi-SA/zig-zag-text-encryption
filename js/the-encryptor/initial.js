20f27870ab082323261c89a9013e6465dbfa7c21
Dedi-SA <coinfoner@gmail.com>
2024-10-22 22:18:06 +0700

    setting the rule: maxPassword & maxReflection

--- js/the-encryptor/initial.js
+++ js/the-encryptor/initial.js
@@ -1,24 +1,42 @@
-// Step 1 : Filtering
-const ZZTE = (theText, thePassword, status) => {
+// Rule
+const maxPassword = 20,
+      maxNumberOfReflection = 100;
+
+// Step 1 : Filtering & Fixing
+const ZZTE = (theText, thePassword, status, numberOfReflections) => {
     if (!is.str(theText)) {
         invArg('ZZTE()');
-        console.log('The input must be string');
+        console.log('The input must be string.');
+        return;
+    }
+    else if(theText.length === 0) {
+        return theText;
+    }
+    else if (typeof thePassword !== 'string') {
+        invArg('ZZTE()');
+        console.log('The password must be string.');
+        return;
+    }
+    else if (typeof numberOfReflections !== 'number') {
+        invArg('ZZTE()');
+        console.log('Number of Reflections must be a number.');
+        return;
     }
     else {
-        if (theText.length === 0) {
-            return theText;
+        // Jika maxNumberOfReflection melebihi batas, maka yang akan digunakan adalah sisa hasil bagi dari nilaiMax
+        // When maxNumberOfReflection exceed the limit, then the remainder will be used
+        if (numberOfReflections > maxNumberOfReflection) {
+
+            numberOfReflections = Math.ceil(numberOfReflections % maxNumberOfReflection);
+
+            console.log('The number of reflections must not exceed ' + maxNumberOfReflection + ', otherwise your results may not be as expected.');
+            console.log('Your Encryption still safe, no problem.');
         }
-        else {
-            if (typeof thePassword !== 'string') {
-                invArg('ZZTE()');
-                console.log('The password must be string');
-            }
-            else if (thePassword.length > 20) {
-                thePassword = thePassword.slice(0, 19);
-            }
-            else {
-                return mulaiEnkripsiDekripsi([...theText], thePassword, status);
-            }
-        }
+
+        // Bila panjang password melebihi batas, maka hanya 20 karakter pertama yang akan digunakan
+        // If the password is longer than the limit, only the first 20 characters will be used
+        thePassword = thePassword.length <= maxPassword ? thePassword : thePassword.slice(0, maxPassword);
+
+        return mulaiEnkripsiDekripsi([...theText], thePassword, status, numberOfReflections);
     }
 };
