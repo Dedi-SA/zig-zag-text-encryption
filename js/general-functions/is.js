99782a8caf8db0c579b780dfb12055c828a50703
Dedi-SA <coinfoner@gmail.com>
2024-10-23 21:44:11 +0700

    add new function: is.number()

--- js/general-functions/is.js
+++ js/general-functions/is.js
@@ -2,4 +2,7 @@ const is = {
     str: str => {
         return typeof str === 'string';
     },
+    number: num => {
+        return typeof num === 'number' && !isNaN(num) && isFinite(num);
+    }
 }
