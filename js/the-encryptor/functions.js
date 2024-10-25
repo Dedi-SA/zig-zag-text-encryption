cdc973fb41b09f9840b8d7901ad2c5f95050166b
Dedi-SA <coinfoner@gmail.com>
2024-10-25 21:35:25 +0700

    add new function: getStringFromMirror()

--- js/the-encryptor/functions.js
+++ js/the-encryptor/functions.js
@@ -14,12 +14,17 @@ const rotate = (theChar, direction, rotationCount) => {
     }
 };
 
-const getRotatorNumbers = (theTextArr, thePassword, sessions) => {
-    const randomNumber_1 = Math.abs(theTextArr.length - thePassword.length) + 1,
-          randomNumber_2 = Math.ceil((thePassword.length + sessions) / 4) + theTextArr.length;
+const getRotatorNumbers = (theTextArr, str, sessions) => {
+    const randomNumber_1 = Math.abs(theTextArr.length - str.length) + 1,
+          randomNumber_2 = Math.ceil((str.length + get.randomNumberByString(str, KEY) + sessions) / 4) + theTextArr.length;
 
     return [
         randomNumber_1 > 10000 ? randomNumber_1 - Math.ceil(randomNumber_1 / 3) + 1 : randomNumber_1,
         randomNumber_2 > 10000 ? randomNumber_2 - Math.floor(randomNumber_2 / 4) + 1 : randomNumber_2
     ];
+};
+
+const getStringFromMirror = () => {
+        return mirrorBox[+get.element('#tempatJenisCermin').value]
+               .slice(7, 3);
 };
