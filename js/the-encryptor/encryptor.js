4f03d49c0782e6e74441f5295e881ba4b6ff15bb
Dedi-SA <coinfoner@gmail.com>
2024-10-25 12:44:42 +0700

    removing required functions to separated file

--- js/the-encryptor/encryptor.js
+++ js/the-encryptor/encryptor.js
@@ -1,28 +1,3 @@
-const rotate = (theChar, direction, rotationCount) => {
-        rotationCount = rotationCount >= KEY.length ? rotationCount % KEY.length : rotationCount;
-
-        if (rotationCount === 0) {
-            return theChar;
-        }
-        else if (direction) {
-            newID = (get.id(theChar, KEY) + rotationCount) % KEY.length;
-            return KEY[newID];
-        }
-        else {
-            newID = (get.id(theChar, KEY) - rotationCount + KEY.length) % KEY.length;
-            return KEY[newID];
-        }
-    };
-
-const getRotatorNumbers = (theTextArr, thePassword, sessions) => {
-        const randomNumber_1 = Math.abs(theTextArr.length - thePassword.length) + 1,
-              randomNumber_2 = Math.ceil((thePassword.length + sessions) / 4) + theTextArr.length;
-
-        return [
-            randomNumber_1 > 10000 ? randomNumber_1 - Math.ceil(randomNumber_1 / 3) + 1 : randomNumber_1,
-            randomNumber_2 > 10000 ? randomNumber_2 - Math.floor(randomNumber_2 / 4) + 1 : randomNumber_2
-        ];
-    };
     
 // Step 2 : Pencerminan, Pembalik, Substitusi ganjil-genap (Mirroring, Reverse, Odd-Even Swap)
 const mulaiEnkripsiDekripsi = (theTextArr, thePassword, status, numberOfReflections) => {
@@ -34,7 +9,6 @@ const mulaiEnkripsiDekripsi = (theTextArr, thePassword, status, numberOfReflecti
             If status === true  : step 2A => step 2B => step 2C => step 3  => End
             If status === false : Step 3  => step 2C => step 2B => step 2A => End
         */
-
         if (status) {
             // Step 2A : Pencerminan (mirroring)
             while (numberOfReflections > 0) {
