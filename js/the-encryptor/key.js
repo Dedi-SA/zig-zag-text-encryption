881be3ca0e828753f77c7803c5232ea163d73643
Dedi-SA <coinfoner@gmail.com>
2024-10-27 10:25:10 +0700

    removing rule part

--- js/the-encryptor/key.js
+++ js/the-encryptor/key.js
@@ -1,5 +1,3 @@
-// Rule
-const KEYmaxLength = 94; // The required number of elements needed for a valid KEY
 
 const KEY = [...'Pr#j["I4YRo.bW;^$v0_h8e`qL}J?u<G1%n]DA7:)N9~Vc-X*tOmi zfx=U{a&6T+EQ\'!2lK|HF>@Sgyk,Ms3dw/Bp(5CZ'],
 
