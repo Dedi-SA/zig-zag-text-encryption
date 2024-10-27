e67d9ccb211c8016b3710b4875f6a9cae8956997
Dedi-SA <coinfoner@gmail.com>
2024-10-27 20:42:29 +0700

    add few additional conditions, add more detailed comment

--- js/the-encryptor/initial.js
+++ js/the-encryptor/initial.js
@@ -1,45 +1,75 @@
 
 // Step 1 : Filtering & Fixing
 const ZZTE = (theText, thePassword, status, numberOfReflections, mirrorId) => {
+
+    // If 'theText' is not a string
     if (!is.str(theText)) {
         invArg('ZZTE()');
         console.log('"theText" should be a string.');
         console.log(`The input : ${typeof theText}`);
         return '';
     }
+    // If 'theText' is empty string, then there is no encryption
     else if(theText.length === 0) {
         return theText;
     }
+    // If 'thePassword' is not a string
     else if (!is.str(thePassword)) {
         invArg('ZZTE()');
         console.log('"thePassword" should be a string.');
         console.log(`The input : ${typeof thePassword}`);
         return '';
     }
-    else if (!is.number(numberOfReflections)) {
+    // If 'numberOfReflections' is not a number or number less than 1
+    else if (!is.number(numberOfReflections) || numberOfReflections < 1) {
         invArg('ZZTE()');
-        console.log('"numberOfReflections" should be a number.');
-        console.log(`The input : ${typeof numberOfReflections}`)
+        console.log('"numberOfReflections" should be a positive number (greater than zero).');
+        console.log(`The input : ${numberOfReflections} (${typeof numberOfReflections})`);
         return '';
     }
+    // If 'numberOfReflections' is larger than the limit
+    else if (numberOfReflections > reflectionMaxNumber) {
+        invArg('ZZTE()');
+        console.log('"numberOfReflections" is invalid.');
+
+        if (reflectionMaxNumber === 1) {
+            console.log('"numberOfReflections" should be 1 because the rule states it must be 1.');
+            console.log('If this amount is too small, please increase the maximum limit by changing the variable.');
+        }
+        else if (reflectionMaxNumber == 2) {
+            console.log('"numberOfReflections" should be 1 or 2 because the maximum number is 2.');
+            console.log('If this amount is too small, please increase the maximum limit by changing the variable.');
+        }
+        else {
+            console.log('"numberOfReflections" should be in the range 1 to ' + reflectionMaxNumber);
+        }
+
+        console.log(`The input : ${numberOfReflections} (${typeof numberOfReflections})`);
+        return '';
+    }
+    // If 'mirrorId' is not a number
     else if (!is.number(mirrorId)) {
         invArg('ZZTE()');
-        console.log('"mirrorId" should be a number.');
-        console.log(`The input : ${typeof mirrorId}`)
+        console.log('"mirrorId" should be a positive number.');
+        console.log(`The input : ${mirrorId} (${typeof mirrorId})`);
         return '';
     }
+    // If 'mirrorId' is negative or beyond the limit
     else if (mirrorId < 0 || mirrorId >= mirrorBox.length) {
         invArg('ZZTE()');
         console.log('"mirrorId" is invalid.');
 
         if (mirrorBox.length === 1) {
-            console.log(`It should be '0' because there is just 1 mirror detected.`);
+            console.log(`It should be 0 because there is just 1 mirror detected.`);
+        }
+        else if (mirrorBox.length === 2) {
+            console.log(`It should be 0 or 1 because there is just 2 mirror detected.`);
         }
         else {
-            console.log(`It should be '0' to ${mirrorBox.length - 1}`);
+            console.log(`It should be between '0' and ${mirrorBox.length - 1}.`);
         }
 
-        console.log(`The input : ${mirrorId}`)
+        console.log(`The input : ${mirrorId} (${typeof mirrorId})`);
         return '';
     }
     else {
@@ -50,7 +80,7 @@ const ZZTE = (theText, thePassword, status, numberOfReflections, mirrorId) => {
             numberOfReflections = Math.ceil(numberOfReflections % reflectionMaxNumber);
 
             console.log('The number of reflections must not exceed ' + reflectionMaxNumber + ', otherwise your results may not be as expected.');
-            console.log('Your Encryption still safe, no problem.');
+            console.log('The Encryption still safe, no problem.');
         }
         
         /*
