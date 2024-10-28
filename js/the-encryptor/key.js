bae2c1ad19790a6cc713d7076978938b3baa2f20
Dedi-SA <coinfoner@gmail.com>
2024-10-28 12:55:24 +0700

    minor change for code standardization

--- js/the-encryptor/key.js
+++ js/the-encryptor/key.js
@@ -4,19 +4,20 @@ const KEY = [...'Pr#j["I4YRo.bW;^$v0_h8e`qL}J?u<G1%n]DA7:)N9~Vc-X*tOmi zfx=U{a&6
       // To Check is variable 'KEY' is valid or not
       isValidKey = theKey => {
             if (Array.isArray(theKey) && theKey.every(e => is.str(e))) {
-                
                 return noDuplicate(theKey).length === KEYmaxLength;
             }
-            else
+            else {
                 return false;
+            }
       },
 
       getTheLostStrings = theKey => {
         let theLostStrings = [];
 
         generalString.forEach(e => {
-            if (theKey.includes(e) === false)
+            if (theKey.includes(e) === false) {
                 theLostStrings.push(e);
+            }
         });
 
         return theLostStrings;
